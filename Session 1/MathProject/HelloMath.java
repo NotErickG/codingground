afd7f08b9ba4210a4f88d386ae6d6ad07bfae7f1
honkyjive <neverhomelong@gmail.com>
2017-05-18 15:36:22 -0400

    Created by Coding Ground Client

---
+++ Session 1/MathProject/HelloMath.java
@@ -0,0 +1,37 @@
+public class HelloMath{
+
+     public static void main(String []args){
+        
+        //This is a comment
+        
+        //these are variables being declared
+        int integerer;      // this is a whole number
+        float floater;      // think smaller decimal numbers
+        double doubler;     // think bigger decimal numbers
+        byte byter;         // minimum value of -128 and a maximum value of 127 (inclusive)
+        short shorter;      // this is like a smaller int
+        boolean booleaner;      // this is true or false
+        char charrer;           // this is generally a single type letter. 'c' or 'C' or '3'
+        
+        
+        // assigning literals
+        integerer = 420000000;
+        floater = 3.14f;
+        doubler = 2.71828182845;
+        byter = -100;
+        shorter = 42;
+        booleaner = true;
+        charrer = 'M';
+        
+        // assigning variables inline example
+        int inlineVariable = 500;    
+        
+        // Maths
+        
+        
+        
+        
+        
+        
+     }
+}
